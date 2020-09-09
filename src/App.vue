f9bec48ad8e8486420b26e568f7c87811e30e455
鞠云鹏 <76542009@qq.com>
2020-09-09 10:46:40 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Docker1"/>
+    <HelloWorld msg="Welcome to Docker2"/>
   </div>
 </template>
 
